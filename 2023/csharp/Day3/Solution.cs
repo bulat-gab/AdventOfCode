cb2ca80438d71ee62357634fb90172fc71d8eebe
bulat-gab <gabdrakhmanov.br@gmail.com>
2023-12-14 10:07:38 +0700

    Add Day 3

---
+++ 2023/csharp/Day3/Solution.cs
@@ -0,0 +1,109 @@
+﻿namespace AdventOfCode.Day3;
+public class Solution
+{
+    private const string Numbers = "0123456789";
+    private const string Symbols = "%/-*$&+#=@";
+
+    private readonly string[] Lines;
+
+    private readonly int Width;
+
+    private readonly HashSet<Number> Visited = [];
+
+    private readonly (int, int)[] Directions =
+    [
+        (-1, -1),
+        (-1,  0),
+        (-1,  1),
+        ( 0, -1),
+        ( 0,  1),
+        ( 1, -1),
+        ( 1,  0),
+        ( 1,  1),
+    ];
+
+    public Solution()
+    {
+        Lines = File.ReadAllLines("./Day3/input.txt");
+        Width = Lines[0].Length;
+    }
+
+    public int PartOne()
+    {
+        return Lines
+            .Select((line, row) => 
+                line.Select((symbol, col) => new { Symbol = symbol, Col = col })
+                    .Where(x => Symbols.Contains(x.Symbol))
+                    .Select(x => GetAdjacent(row, x.Col).Sum(x => x.Value))
+                    .Sum())
+            .Sum();
+    }
+
+    /*
+    * Gear == symbol * adjacent to exactly 2 numbers
+    * Gear ratio  == multiple of those 2 numbers
+    * Find sum of all gear ratios
+    *
+    **/
+    public int PartTwo()
+    {
+        return Lines
+            .Select((line, row) =>
+                line.Select((symbol, col) => new { Symbol = symbol, Col = col })
+                    .Where(x => x.Symbol == '*')
+                    .Select(x => GetAdjacent(row, x.Col).ToArray())
+                    .Where(x => x.Length == 2)
+                    .Select(x => x[0].Value * x[1].Value)
+                    .Sum())
+            .Sum();
+    }
+
+    private IEnumerable<Number> GetAdjacent(int row, int col)
+    {
+        foreach (var (dx, dy) in Directions)
+        {
+            var x = dx + col;
+            var y = dy + row;
+
+            if (IsValidCoordinate(x, y) && char.IsNumber(Lines[y][x]))
+            {
+                var number = ParseNumber(x, y);
+                if (!Visited.Contains(number))
+                {
+                    Visited.Add(number);
+                    yield return number;
+                }
+            }
+        }
+    }
+
+    public Number ParseNumber(int x, int y)
+    {
+        var line = Lines[y];
+
+        var left = x;
+        var right = x;
+
+        while (left > -1 && char.IsNumber(line[left]))
+        {
+            left--;
+        }
+
+        while (right < Width && char.IsNumber(line[right]))
+        {
+            right++;
+        }
+
+        left++;
+        var numberString = line[left..right];
+        return new Number(int.Parse(numberString), (y, left, right));
+    }
+
+    private bool IsValidCoordinate(int dx, int dy)
+    {
+        return 0 <= dx && dx < Width 
+            && 0 <= dy && dy < Lines.Length;
+    }
+
+    public record Number(int Value, (int Row, int X, int Y) Coordinate);
+}
--- 2023/csharp/Program.cs
+++ 2023/csharp/Program.cs
@@ -1,37 +1,20 @@
 ﻿using System.Diagnostics;
 
-//new Day1().PartOne();
-//new Day1().PartTwo();
 
-//new Day4().PartOne();
-
-//new AdventOfCode.Day6.Part1().PartOne();
 
 Stopwatch sw = Stopwatch.StartNew();
 
-//new AdventOfCode.Day6.Part2().PartTwo();
+var result = new AdventOfCode.Day3.Solution().PartOne();
+Console.WriteLine("Part One Answer: " + result);
 
-//Console.WriteLine("=====================================");
-//Console.WriteLine("=====================================");
-//Console.WriteLine("Elapsed ms={0}", sw.Elapsed.TotalMilliseconds);
-
-
-
-
-var result = new AdventOfCode.Day2.Solution().PartOne();
-Console.WriteLine("Answer: " + result);
-if (result != 2169)
+if (result != 559667)
 {
     throw new Exception();
 }
 
-
-
-
-
-result = new AdventOfCode.Day2.Solution().PartTwo();
-Console.WriteLine("Answer: " + result);
-if (result != 60948)
+var r2 = new AdventOfCode.Day3.Solution().PartTwo();
+Console.WriteLine("Part Two Answer: " + r2);
+if (r2 != 86841457)
 {
     throw new Exception();
 }
@@ -42,3 +25,25 @@ sw.Stop();
 Console.WriteLine("=====================================");
 Console.WriteLine("=====================================");
 Console.WriteLine("Elapsed ms={0}", sw.Elapsed.TotalMilliseconds);
+
+
+//var line = "..35..633.";
+//var start = 2;
+//var end = 3;
+//var Width = line.Length;
+
+//var truncatedStart = Math.Max(start, 0);
+//var truncatedEnd = Math.Min(end + 1, Width - 1);
+
+//var substring = line[truncatedStart..truncatedEnd];
+
+//Console.WriteLine(substring);
+
+//var x1 = new AdventOfCode.Day3.Solution2().ParseNumber(0, 0);
+//Console.WriteLine(x1);
+
+//var x2 = new AdventOfCode.Day3.Solution2().ParseNumber(5, 0);
+//Console.WriteLine(x2);
+
+//var x3 = new AdventOfCode.Day3.Solution2().ParseNumber(9, 0);
+//Console.WriteLine(x3);
