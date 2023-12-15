4f07df5324ce1c40ca8d9b65b32c196e09329490
bulat-gab <gabdrakhmanov.br@gmail.com>
2023-12-15 12:00:23 +0700

    Add Day 9

---
+++ 2023/csharp/Day9/Solution.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2023.Day9;
+public class Solution
+{
+    private readonly string[] Lines;
+
+    public Solution()
+    {
+        Lines = File.ReadAllLines("./Day9/input.txt");
+    }
+
+    public int PartOne()
+    {
+        return Lines
+            .Select(line => line.Split(" ").Select(int.Parse).ToList())
+            .Select(FillSequences)
+            .Select(x => ExtrapolateRight(x))
+            .Select(x => x[0][^1])
+            .Sum();
+    }
+
+    public int PartTwo()
+    {
+        return Lines
+            .Select(line => line.Split(" ").Select(int.Parse).ToList())
+            .Select(FillSequences)
+            .Select(x => ExtrapolateLeft(x))
+            .Select(x => x[0][0])
+            .Sum();
+    }
+
+    private List<List<int>> ExtrapolateRight(List<List<int>> sequences)
+    {
+        for (var sequenceIndex = sequences.Count() - 1; sequenceIndex > 0; sequenceIndex--)
+        {
+            var bottom = sequences[sequenceIndex][^1];
+            var left = sequences[sequenceIndex - 1][^1];
+
+            int newValue = left + bottom;
+
+
+            sequences[sequenceIndex - 1].Add(newValue);
+        }
+
+        return sequences;
+    }
+
+
+    private List<List<int>> ExtrapolateLeft(List<List<int>> sequences)
+    {
+        for (var sequenceIndex = sequences.Count() - 1; sequenceIndex > 0; sequenceIndex--)
+        {
+            var bottom = sequences[sequenceIndex][0];
+            var left = sequences[sequenceIndex - 1][0];
+
+            var newValue = left - bottom;
+
+            sequences[sequenceIndex - 1].Insert(0, newValue);
+
+        }
+
+        return sequences;
+    }
+
+    private List<List<int>> FillSequences(List<int> input)
+    {
+        var sequences = new List<List<int>>
+        {
+            input
+        };
+
+        int sequenceIndex = 0;
+
+        while (true)
+        {
+            var tempList = new List<int>();
+
+            for (int i = 1; i < sequences[sequenceIndex].Count; i++)
+            {
+                var difference = sequences[sequenceIndex][i] - sequences[sequenceIndex][i - 1];
+                tempList.Add(difference);
+            }
+
+            sequences.Add(tempList);
+            sequenceIndex++;
+            if (tempList.All(x => x == 0))
+            {
+                break;
+            }
+        }
+
+        return sequences.ToList();
+    }
+}
--- 2023/csharp/Program.cs
+++ 2023/csharp/Program.cs
@@ -10,17 +10,34 @@ Stopwatch sw = Stopwatch.StartNew();
 //    Console.WriteLine("Part 1 Invalid Answer");
 //}
 
-var r2 = new AdventOfCode2023.Day1.Day1().PartTwo();
-Console.WriteLine("Answer Part 2: " + r2);
-if (r2 != 281)
+//var r2 = new AdventOfCode2023.Day1.Day1().PartTwo();
+//Console.WriteLine("Answer Part 2: " + r2);
+//if (r2 != 281)
+//{
+//    Console.WriteLine("Part 2 Invalid Answer");
+//}
+
+
+var r1 = new AdventOfCode2023.Day9.Solution().PartOne();
+Console.WriteLine("r1: " + r1);
+if (r1 != 1757008019)
+{
+    Console.WriteLine("Part 1 Invalid Answer");
+}
+
+Console.WriteLine(  );
+
+var r2 = new AdventOfCode2023.Day9.Solution().PartTwo();
+Console.WriteLine("r2: " + r2);
+if (r2 != 995)
 {
     Console.WriteLine("Part 2 Invalid Answer");
 }
 
 
 
+
 sw.Stop();
-Console.WriteLine("=====================================");
 Console.WriteLine("=====================================");
 Console.WriteLine("Elapsed ms={0}", sw.Elapsed.TotalMilliseconds);
 
