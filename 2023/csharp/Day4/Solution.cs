ae6f94ae55e5cce2ac5e4ccba2f8b5f2278f151c
bulat-gab <gabdrakhmanov.br@gmail.com>
2023-12-18 14:44:26 +0700

    Add Day 4 Part 2

---
+++ 2023/csharp/Day4/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023.Day4;
+public class Solution
+{
+    public int PartOne()
+    {
+        var lines = File.ReadAllLines("./Day4/input.txt");
+
+        return lines
+            .Select(GetNumberOfMatches)
+            .Select(matches => (int)Math.Pow(2, matches - 1))
+            .Sum();
+    }
+
+    public int PartTwo()
+    {
+        var lines = File.ReadAllLines("./Day4/input.txt");
+
+        int[] counts = Enumerable.Repeat(1, lines.Length).ToArray();
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var matches = GetNumberOfMatches(lines[i]);
+
+            var numberOfCopies = counts[i];
+
+            // add winning copy
+            while (matches > 0)
+            {
+                if (matches + i < counts.Length)
+                {
+                    counts[i + matches] = counts[i + matches] + numberOfCopies;
+                }
+                matches--;
+            }
+        }
+
+        return counts.Sum();
+    }
+
+    private static int GetNumberOfMatches(string line)
+    {
+        var parts = line.Split(':', '|');
+        var winningCards = ParseCards(parts[1]);
+        var myCards = ParseCards(parts[2]);
+
+        return winningCards
+            .Intersect(myCards)
+            .Count();
+    }
+
+    private static IEnumerable<string> ParseCards(string cards)
+        => Regex.Matches(cards, @"\d+").Select(x => x.Value);
+}
--- 2023/csharp/Program.cs
+++ 2023/csharp/Program.cs
@@ -4,16 +4,16 @@
 
 Stopwatch sw = Stopwatch.StartNew();
 
-var r1 = new AdventOfCode2023.Day16.Solution().PartOne();
+var r1 = new AdventOfCode2023.Day4.Solution().PartOne();
 Console.WriteLine("R1: " + r1);
-if (r1 != 8901)
+if (r1 != 17782)
 {
     throw new Exception();
 }
 
-var r2 = new AdventOfCode2023.Day16.Solution().PartTwo();
-Console.WriteLine("R: " + r2);
-if (r2 != 9064)
+var r2 = new AdventOfCode2023.Day4.Solution().PartTwo();
+Console.WriteLine("R2: " + r2);
+if (r2 != 8477787)
 {
     throw new Exception();
 }
