aac98a555f32bd7138fbd4312e4f77c827a9d679
bulat-gab <gabdrakhmanov.br@gmail.com>
2023-12-18 09:26:52 +0700

    Add day 0 as a template

---
+++ 2023/csharp/Day0/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2023.Day0;
+public class Solution
+{
+    private readonly string[] Lines;
+
+    public Solution()
+    {
+        Lines = File.ReadAllLines("./Day/input.txt");
+    }
+
+    public int PartOne()
+    {
+        return 0;
+    }
+
+    public int PartTwo()
+    {
+        return 0;
+    }
+}
